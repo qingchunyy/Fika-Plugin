4e269104930b674a01f8ab248047de14e1fd254c
qingchunyy <2656469055@qq.com>
2026-06-27 21:20:49 +0800

    Update TarkovApplication_LocalGameCreator_Patch.cs

--- Fika.Core/Main/Patches/LocalGame/TarkovApplication_LocalGameCreator_Patch.cs
+++ Fika.Core/Main/Patches/LocalGame/TarkovApplication_LocalGameCreator_Patch.cs
@@ -155,14 +155,14 @@ public sealed class TarkovApplication_LocalGameCreator_Patch : ModulePatch
 
         if (!isServer)
         {
-            instance.MatchmakerPlayerControllerClass.UpdateMatchingStatus("Joining coop game...");
+            instance.MatchmakerPlayerControllerClass.UpdateMatchingStatus("正在加入合作游戏...");
 
             RaidSettingsRequest data = new();
             var raidSettingsResponse = await FikaRequestHandler.GetRaidSettings(data);
 
             if (!raidSettingsResponse.Received)
             {
-                throw new InvalidDataException("Failed to retrieve raid settings");
+                throw new InvalidDataException("获取战局设置失败");
             }
 
             raidSettings.MetabolismDisabled = raidSettingsResponse.MetabolismDisabled;
@@ -175,7 +175,7 @@ public sealed class TarkovApplication_LocalGameCreator_Patch : ModulePatch
         }
         else
         {
-            instance.MatchmakerPlayerControllerClass.UpdateMatchingStatus("Hosting coop game...");
+            instance.MatchmakerPlayerControllerClass.UpdateMatchingStatus("正在创建合作游戏...");
             Singleton<FikaServer>.Instance.LocationReceived = true;
         }
 
@@ -206,11 +206,11 @@ public sealed class TarkovApplication_LocalGameCreator_Patch : ModulePatch
         ScreenUpdater updater = new(instance.MatchmakerPlayerControllerClass, coopGame);
         if (!isServer)
         {
-            coopGame.SetMatchmakerStatus("Coop game joined");
+            coopGame.SetMatchmakerStatus("已加入合作游戏");
         }
         else
         {
-            coopGame.SetMatchmakerStatus("Coop game created");
+            coopGame.SetMatchmakerStatus("已创建合作游戏");
         }
 
         await coopGame.InitPlayer(raidSettings.BotSettings);
